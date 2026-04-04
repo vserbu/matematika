98eb207cfdb4d1c43261cd82d0e57535aba0332f
vserbu <57716396+vserbu@users.noreply.github.com>
2026-04-04 16:17:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'matematika-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/matematika/',
+  '/matematika/index.html',
+  '/matematika/manifest.json',
+  '/matematika/icon-192.png',
+  '/matematika/icon-512.png'
 ];
 
 // Install service worker and cache files
